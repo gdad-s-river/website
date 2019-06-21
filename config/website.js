bbb4d2ca9a61068ae6a5064b1c4d579190641ef3
gdad-s-river <arihantverma1994@gmail.com>
2019-06-21 20:28:08 +0530

    init commit

---
+++ config/website.js
@@ -0,0 +1,28 @@
+module.exports = {
+  pathPrefix: '/blog/work', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
+  siteTitle: 'The Work Blog', // Navigation and Site Title
+  siteTitleAlt: 'Musings About Working Where I Do', // Alternative Site title for SEO
+  siteTitleShort: 'The Work Blog', // short_name for manifest
+  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: `I took a leave one day. A work mail came and I realised how "frightened" that mail was about owning up and just sharing work concern. I thought that and things at work are worth writing about. This is the place about those things`,
+  author: 'Arihant Verma', // Author for schemaORGJSONLD
+  organization: 'Arihant Verma',
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@gdad_s_river', // Twitter Username
+  ogSiteName: `Arihant's Work Blog`, // Facebook Site Name
+  ogLanguage: 'en_US',
+  googleAnalyticsID: '',
+
+  // Manifest and Progress color
+  themeColor: '#5348FF',
+  backgroundColor: '#2b2e3c',
+
+  // Social component
+  twitter: 'https://twitter.com/gdad_s_river/',
+  twitterHandle: '@gdad_s_river',
+  github: 'https://github.com/arihantverma/',
+  linkedin: 'https://www.linkedin.com/in/arihantverma/',
+}
